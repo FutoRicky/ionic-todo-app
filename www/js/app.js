363da0207c72be3a424950b63bda0156c9a48576
futoricky <ricardoamercado@gmail.com>
2016-01-25 23:46:40 -0400

    display list items, add create task functionality

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,33 @@
-// Ionic Starter App
+// Ionic todo-app App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'todo-app' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('todo-app', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  $scope.tasks = [];
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up',
   });
-})
+
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title,
+    });
+    $scope.taskModal.hide();
+    task.title = '';
+  };
+
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+});
